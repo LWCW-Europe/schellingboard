f3bd1fcd932de079cf855196613fb04f671b547f
aggalex <ubuntu1aggelalex@gmail.com>
2026-07-22 20:36:17 +0300

    feat: implement keyboard shortcuts for markdown editor
    Issue #600

--- app/components/markdown-options.ts
+++ app/components/markdown-options.ts
@@ -10,10 +10,13 @@ import {
 } from "@heroicons/react/16/solid";
 
 type Icon = typeof BoldIcon;
+type Key =
+  RegExp | string | ((e: React.KeyboardEvent<HTMLTextAreaElement>) => boolean);
 type Option = {
   icon: Icon;
   label: string;
   template: Template;
+  key: Key;
 };
 
 /**
@@ -180,6 +183,19 @@ const around =
     return tpl`${newBefore}${newSelectedText}${newAfter}`;
   };
 
+export const isKey =
+  (key: Key) => (e: React.KeyboardEvent<HTMLTextAreaElement>) =>
+    typeof key === "function"
+      ? key(e)
+      : typeof key === "string"
+        ? e.key.toLowerCase() === key.toLowerCase()
+        : key.test(e.key);
+
+const ctrl = (k: Key) => (e: React.KeyboardEvent<HTMLTextAreaElement>) =>
+  (e.ctrlKey || e.metaKey) && isKey(k)(e);
+const shift = (k: Key) => (e: React.KeyboardEvent<HTMLTextAreaElement>) =>
+  e.shiftKey && isKey(k)(e);
+
 export const options: Option[] = [
   {
     icon: BoldIcon,
@@ -187,6 +203,7 @@ export const options: Option[] = [
     template: around(
       (before, selectedText, after) => tpl`${before}**${selectedText}**${after}`
     ),
+    key: ctrl("B"),
   },
   {
     icon: ItalicIcon,
@@ -194,6 +211,7 @@ export const options: Option[] = [
     template: around(
       (before, selectedText, after) => tpl`${before}*${selectedText}*${after}`
     ),
+    key: ctrl("I"),
   },
   {
     icon: ChatBubbleBottomCenterTextIcon,
@@ -203,6 +221,7 @@ export const options: Option[] = [
         (before, selectedText, after) => tpl`${before}> ${selectedText}${after}`
       )
     ),
+    key: ctrl("Q"),
   },
   {
     icon: NumberedListIcon,
@@ -217,11 +236,13 @@ export const options: Option[] = [
         (line, index) => `${index + 1 + lastNumber}. ${line}`
       )(before, selectedText, after);
     }),
+    key: ctrl(/\d/),
   },
   {
     icon: ListBulletIcon,
     label: "Bullet list (Ctrl+-)",
     template: selectLine(listMarker(/^-\s+/, (line) => `- ${line}`)),
+    key: ctrl("-"),
   },
   {
     icon: CodeBracketIcon,
@@ -229,6 +250,7 @@ export const options: Option[] = [
     template: around(
       (before, selectedText, after) => tpl`${before}\`${selectedText}\`${after}`
     ),
+    key: ctrl("`"),
   },
   {
     icon: CodeBracketSquareIcon,
@@ -239,6 +261,7 @@ export const options: Option[] = [
           tpl`${before}\`\`\`\n${selectedText}\n\`\`\`${after}`
       )
     ),
+    key: ctrl(shift("~")),
   },
   {
     icon: LinkIcon,
@@ -246,5 +269,6 @@ export const options: Option[] = [
     template: around(
       (before, selectedText, after) => tpl`${before}[](${selectedText})${after}`
     ),
+    key: ctrl("L"),
   },
 ];
